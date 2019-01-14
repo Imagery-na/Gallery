6c2abbd53816eef2cd471f6e0f3e3c15809ea810
Marina <38287103+Imagery-na@users.noreply.github.com>
2019-01-14 19:32:00 +1000

    Сайт с моими картинами

---
+++ script.js
@@ -0,0 +1,7 @@
+function openImageWindow(src) {
+    var image = new Image();
+    image.src = src;
+    var width = image.width;
+    var height = image.height;
+    window.open(src,"Image","width=" + width + ",height=" + height);
+  }
