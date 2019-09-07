c8be7315bbd101a0900010c2e624b97267ab5569
SportProject <38287103+Imagery-na@users.noreply.github.com>
2019-09-07 23:30:07 +1000

    Переделываю

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-function openImageWindow(src) {
+/*function openImageWindow(src) {
     var image = new Image();
     image.src = src;
     var width = image.width;
     var height = image.height;
     window.open(src,"Image","width=" + width + ",height=" + height);
-  }
+  } */
+
+  const pictures = document.querySelectorAll('.pict');
+  console.log(pictures);
+  const modal = document.getElementById('modal');
+  modal.style.display = 'none';
+  console.log(modal);
+  pictures.forEach((pict)=> {
+    pict.addEventListener('click', ()=>{
+      modal.style.display = '';
+    });
+  });
