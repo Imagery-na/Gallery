aee50ff65fe30bf9b23a6fd3d8a9b3202a66c75a
Marina <38287103+Imagery-na@users.noreply.github.com>
2019-12-12 00:01:19 +1000

    Сделала адаптивность

--- script.js
+++ script.js
@@ -6,36 +6,42 @@
     window.open(src,"Image","width=" + width + ",height=" + height);
   } */
 
-  const pictures = document.querySelectorAll('.pict');
-  console.log(pictures);
-  const modal = document.getElementById('modal');
-  const blackFill = document.getElementById('black_fill');
-  const pictBox = document.querySelector('.bigPict');
-  modal.style.display = 'none';
-  console.log(modal);
-  pictures.forEach((pict)=> {
-    pict.addEventListener('click', ()=>{
-      const pictClone = pict.cloneNode(true);
-      pictBox.appendChild(pictClone);
-      pictBox.style.height='80vh';
-      modal.style.display = 'flex';
-      document.body.style.overflow = 'hidden';
-      blackFill.classList.add('open');
-      pictBox.querySelector('.pict').style.height = '80vh';
-      pictBox.querySelector('.pict').style.width = 'auto';
-    });
+const pictures = document.querySelectorAll('.pict');
+console.log(pictures);
+const modal = document.getElementById('modal');
+const blackFill = document.getElementById('black_fill');
+const pictBox = document.querySelector('.bigPict');
+console.log(modal);
+pictures.forEach((pict) => {
+  pict.addEventListener('click', () => {
+    const pictClone = pict.cloneNode(true);
+    pictBox.appendChild(pictClone);
+    modal.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+    blackFill.classList.add('open');
+    if(pictClone.classList.contains('imgvertic')){
+      pictClone.classList.add('BigImgVertic');
+      modal.classList.add('ModalForImgVertic');
+    }
+      pictClone.classList.add('BigImgGoriz');
   });
-  const closeBtn = document.querySelector('.closeBtn');
-  closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
-    document.body.style.overflow = ''; // скроллится
-    blackFill.classList.remove('open');
-    while (pictBox.firstChild) {
-      pictBox.removeChild(pictBox.firstChild);
+});
+const closeBtn = document.querySelector('.closeBtn');
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+  document.body.style.overflow = ''; // скроллится
+  blackFill.classList.remove('open');
+  modal.classList.remove('ModalForImgVertic');
+  while (pictBox.firstChild) {
+    pictBox.removeChild(pictBox.firstChild);
   }
 });
 blackFill.addEventListener('click', () => {
   modal.style.display = 'none';
   document.body.style.overflow = '';
   blackFill.classList.remove('open');
+  modal.classList.remove('ModalForImgVertic');
+  while (pictBox.firstChild) {
+    pictBox.removeChild(pictBox.firstChild);
+  }
 });
